167deecf24648d47f6bb9983eb42c2e1ae982255
lrodeveloperr <lrodeveloperr@gmail.com>
2026-08-24 17:09:39 -0700

    Fix Compose dp import for APK compile

--- app/src/main/java/studio/gooduse/kitchenprep/KitchenPrepClosedTestApp.kt
+++ app/src/main/java/studio/gooduse/kitchenprep/KitchenPrepClosedTestApp.kt
@@ -19,6 +19,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
